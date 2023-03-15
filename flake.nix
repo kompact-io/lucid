8c37441c0fcc4f3dece547c2587e17bee4b8b001
waalge <waagle@jes>
2023-03-15 11:35:36 +0000

    alejandra nix

--- flake.nix
+++ flake.nix
@@ -4,28 +4,26 @@
   inputs = {
     devshell.url = "github:numtide/devshell";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url="github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { self, ...} @ inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system: 
-        let pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [ 
-            inputs.devshell.overlay 
-            ];
-        };
-        in
-     {
-      devShell =
-        pkgs.devshell.mkShell {
-          name="lucid";
-          packages = with pkgs; [
-            deno
-            rnix-lsp
-          ];
+  outputs = {self, ...} @ inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          inputs.devshell.overlay
+        ];
+      };
+    in {
+      devShell = pkgs.devshell.mkShell {
+        name = "lucid";
+        packages = with pkgs; [
+          deno
+          rnix-lsp
+        ];
         commands = [
         ];
-        };
+      };
     });
 }
